4d6f73db79ff0adbf0f1bdfaf9f4278ac5991410
Juan Daniel Fiallos <juandanielfiallos@gmail.com>
2024-03-04 21:33:18 -0600

    UPDATE - Libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'hybrid'
+  integrations: [
+    tailwind(),
+    preact(),
+  ],
+  output: 'hybrid',
 });
